3dca4ff10a85c736cfc45f4e40a198aa4e2f83ca
Peng-Yu Chen <walkccray@gmail.com>
2022-01-09 22:01:01 -0500

    feat: add telescope to find/grep files

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -88,6 +88,9 @@ return packer.startup(function(use)
   -- Null LS
   use 'jose-elias-alvarez/null-ls.nvim' -- for formatters and linters
 
+  -- Telescope
+  use { 'nvim-telescope/telescope.nvim', requires = { 'nvim-lua/plenary.nvim' } }
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
