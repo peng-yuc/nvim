e15a059778387440e36e91227b765bd5fd483fb8
Peng-Yu Chen <walkccray@gmail.com>
2022-01-09 22:05:38 -0500

    feat: add toggleterm

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -109,6 +109,9 @@ return packer.startup(function(use)
   -- Git Sign
   use 'lewis6991/gitsigns.nvim'
 
+  -- Toggle Terminal
+  use 'akinsho/toggleterm.nvim'
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
