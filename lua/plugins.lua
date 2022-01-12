d6db7715c129996ca51634a7e6a7378d0dfa1ce8
Peng-Yu Chen <walkccray@gmail.com>
2022-01-12 18:17:32 -0500

    feat: add lualine

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -112,6 +112,9 @@ return packer.startup(function(use)
   -- Toggle Terminal
   use 'akinsho/toggleterm.nvim'
 
+  -- Lualine
+  use 'nvim-lualine/lualine.nvim'
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
